476d76d9f98728b004fdaf41e213077933444efb
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-10-19 13:41:17 -0700

    Complete iteration 3

--- lib/team.rb
+++ lib/team.rb
@@ -11,4 +11,40 @@ class Team
     def add_player(player)
       @roster << player
     end
-end
+
+    def long_term_players
+      players = []
+      roster.each do |player|
+        if player.contract_length > 24 then
+          players << player
+        end
+      end
+      players
+    end
+
+    def short_term_players
+      players = []
+      roster.each do |player|
+        if player.contract_length <= 24 then
+          players << player
+        end
+      end
+      players
+    end
+
+    def total_value
+      total = 0
+        roster.each do |player|
+        total += (player.monthly_cost * player.contract_length)
+        end
+        total
+    end
+
+    def details
+      deets = {
+        "total_value" => self.total_value,
+        "player_count" => self.player_count
+      }
+      deets
+    end
+  end
